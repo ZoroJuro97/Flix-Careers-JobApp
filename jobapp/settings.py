a8225637368e797a6e6924d2d2d18ae5998a6824
ZoroJuro97 <negiabhishek97@gmail.com>
2023-05-10 19:03:23 +0530

    Allowed Hosts settings changed 5

--- jobapp/settings.py
+++ jobapp/settings.py
@@ -25,11 +25,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("IS_DEVELOPMENT", True) == 'True'
 
-ALLOWED_HOSTS = []
-
-RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
-if RENDER_EXTERNAL_HOSTNAME:
-    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
