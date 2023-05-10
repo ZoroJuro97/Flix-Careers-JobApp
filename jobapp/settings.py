56dadc1bc644ac8fdf2368a609ffa73c9bbcbcc0
ZoroJuro97 <negiabhishek97@gmail.com>
2023-05-10 12:10:07 +0530

    changed settings

--- jobapp/settings.py
+++ jobapp/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY", 'django-insecure-)y!8&1co7i*-s2+eul9vsclek6zvl+7)rcni%$h3ua4b4m&vu^')
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("IS_DEVELOPMENT", True) == 'True'
