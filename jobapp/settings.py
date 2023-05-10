612227a06e8d4b513ac305dc79eae7404272ef09
ZoroJuro97 <negiabhishek97@gmail.com>
2023-05-10 19:52:08 +0530

    PostgreSql integrated

--- jobapp/settings.py
+++ jobapp/settings.py
@@ -16,6 +16,10 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+import environ
+env = environ.Env()
+environ.Env.read_env()
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
@@ -75,13 +79,21 @@ WSGI_APPLICATION = 'jobapp.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+'''
+
+#PostgreSQL Integration
+import dj_database_url
+DATABASES = {
+    'default' : dj_database_url.parse(env('DATABASE_URL'))
+}
+
 
 
 # Password validation
